847c5acb49b62c667379bbdce15cf003b1043d86
Niklas Choinowski <niklas.choinowski@gmail.com>
2022-12-17 14:06:33 +0100

    Event Update
    Updated the events where when a user join the server he gets an welcome message into the designated channel and a goodbye message when he leaves the server

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -2,13 +2,14 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName("invite")
+    .setName("setup")
     .setNameLocalizations({
         "de": "einladung",
     })
     .setDescription("Get the invite link for the server")
     .setDescriptionLocalizations({
         "de": "Erhalte den Einladungslink für den Server",
+        "en-US": "Get the invite link for the server",
     }),
     async execute(interaction) {
         await interaction.reply("https://discord.gg/KjrqqfQWdX");
