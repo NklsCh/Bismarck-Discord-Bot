2ba7e641905b49a6950b10d5bccb4f112e9a3ea5
Niklas Choinowski <niklas.choinowski@gmail.com>
2022-12-17 02:32:05 +0100

    Handler Update
    Made an event handler and outsourced the events in their own  files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const fs = require("fs");
-const { Client, Collection, ActivityType, GatewayIntentBits, PermissionsBitField} = require("discord.js");
+const path = require('node:path');
+const { Client, Collection, GatewayIntentBits} = require("discord.js");
 
 const client = new Client({
     intents: [GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences, GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions]
@@ -15,105 +16,28 @@ commandFiles.forEach(commandFile => {
     client.commands.set(command.data.name, command);
 })
 
-const serverAmount = client.guilds.cache;
+const eventsPath = path.join(__dirname, './../events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once("ready", () => {
-    console.log("Bot is ready!");;
-    client.user.setPresence({
-        activities: [{
-            name: `${serverAmount.size} Server(s)`,
-            type: ActivityType.Watching
-        }],
-        status: "online"
-    })
-    //Get the config file for the server and change the channel name to the amount of users in the server
-    client.guilds.cache.forEach(guild => {
-        if(fs.existsSync('./server-configs/' + guild.id + '.json') === false) return;
-        //Get all online users from guild
-        let onlineUsers = guild.members.cache.filter(member => member.presence?.status === "online" && !member.user.bot).size;
-        let config = JSON.parse(fs.readFileSync('./server-configs/' + guild.id + '.json', 'utf8'));
-        if(!config.onlineChannel) return;
-        guild.channels.edit(config.onlineChannel, {name: `Online: ${onlineUsers}`});
-        if(!config.allChannel) return;
-        guild.channels.edit(config.allChannel, {name: `Members: ${guild.memberCount}`});
-    })
-});
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
 
-client.on("guildCreate", guild => {
-    client.user.setPresence({
-        activities: [{
-            name: `${serverAmount.size} Server(s)`,
-            type: ActivityType.Watching
-        }],
-        status: "online"
-    })
-    fs.writeFileSync('./server-configs/' + guild.id + '.json', JSON.stringify({
-        "name": guild.name,
-    }))
-});
+/* const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-client.on("guildDelete", guild => {
-    client.user.setPresence({
-        activities: [{
-            name: `${serverAmount.size} Server(s)`,
-            type: ActivityType.Watching
-        }],
-        status: "online"
-    })
-});
-
-client.on("interactionCreate", async interaction => {
-    if (!interaction.isCommand() && !interaction.isButton()) return;
-    
-    const command = client.commands.get(interaction.commandName);
-    
-    if (command) {
-        try {
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            await interaction.reply({ content: "There was an error while executing this command!", ephemeral: true });
-        }
-    } 
-    
-    if(interaction.isButton()) {
-        let isAdmin
-        try {
-            isAdmin = interaction.member.permissions.has(PermissionsBitField.ADMINISTRATOR);
-        } catch (error) {
-            isAdmin = false;
-        }
-        userid = interaction.message.embeds.map(embed => embed.footer.text)
-        let user = interaction.guild.members.cache.find(user => user.id === userid[0])
-        switch(interaction.customId) {
-            case "kick":
-                if(interaction.guild.members.cache.get(user.id).kickable) {
-                    try {
-                        await interaction.guild.members.cache.get(user.id).kick();
-                    } catch (error) {}
-                } else {
-                    await interaction.reply({ephemeral: true, content: "I can't kick this user"});
-                }
-                interaction.reply({ephemeral: true, content: `The user ${user} has been kicked`});
-                break;
-            case "ban":
-                if(interaction.guild.members.cache.get(user.id).bannable) {
-                    try {
-                        await interaction.guild.members.cache.get(user.id).ban();
-                    } catch (error) {}
-                } else {
-                    await interaction.reply({ephemeral: true, content: "I can't ban this user"});
-                }
-                interaction.reply({ephemeral: true, content: `The user ${user} has been banned`});
-                break;
-            default:
-                await interaction.reply({ephemeral: true, content: "Button Clicked!"});
-                break;
-        }
-                
+eventFiles.forEach(file => {
+    const event = require(`./../events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
-
-});
-
+}); */
 
 client.login(process.env.TOKEN);
