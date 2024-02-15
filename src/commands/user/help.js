1fa07bca611386906e77879eb09fb694d2a52c5d
Niklas Choinowski <niklas.choinowski@gmail.com>
2024-02-15 11:16:07 +0100

    Refactor makeHelpEmbed function to accept an array of commands and folder name

--- src/commands/user/help.js
+++ src/commands/user/help.js
@@ -3,7 +3,9 @@ const {
     SlashCommandBuilder,
     REST,
     Routes,
+    Embed,
 } = require('discord.js')
+const fs = require('fs')
 const makeHelpEmbed = require('../../functions/helpEmbedCreator')
 
 module.exports = {
@@ -28,61 +30,74 @@ module.exports = {
             .setDescription('Here are all commands\r------------------')
             .setColor('Blue')
 
-        commands.forEach((command) => {
-            if (command.name === 'Info') return
-            
-            const isBaseCommand =
-                command.options && command.options.length === 0
-            const isSubcommandGroup =
-                command.options &&
-                command.options.some((option) => option.type === 2)
-            const isToplevelCommand = !isBaseCommand && !isSubcommandGroup
+        const commandFolders = fs.readdirSync('./src/commands')
 
-            if (isToplevelCommand) {
-                helpEmbed.addFields({
-                    name: `</${command.name}:${command.id}>`
-                        ? `</${command.name}:${command.id}>`
-                        : 'No name',
-                    value: `${
-                        command.description
-                            ? command.description
-                            : 'No description'
-                    }`,
-                })
-            }
-            command.options?.forEach((option) => {
-                if (option.type == 2) {
-                    option.options?.forEach((subOption) => {
-                        helpEmbed.addFields({
-                            name: `</${
-                                command.name +
-                                ' ' +
-                                option.name +
-                                ' ' +
-                                subOption.name
-                            }:${command.id}>`
-                                ? `</${
-                                      command.name +
-                                      ' ' +
-                                      option.name +
-                                      ' ' +
-                                      subOption.name
-                                  }:${command.id}>`
+        const embeds = []
+        commandFolders.forEach((folder) => {
+
+            const commandf = fs.readdirSync(`./src/commands/${folder}`)
+                .filter((file) => file.endsWith('.js'))
+
+            let embed = makeHelpEmbed(interaction, folder)
+
+            commandf.forEach((file) => {
+                f = require(`../../commands/${folder}/${file}`)
+
+                commands.forEach((command) => {
+                    if (command.name === 'Info' || f.data.name !== command.name) return
+
+                    const isBaseCommand =
+                        command.options && command.options.length === 0
+                    const isSubcommandGroup =
+                        command.options &&
+                        command.options.some((option) => option.type === 2)
+                    const isToplevelCommand = !isBaseCommand && !isSubcommandGroup
+
+                    if (isToplevelCommand) {
+                        embed.addFields({
+                            name: `</${command.name}:${command.id}>`
+                                ? `</${command.name}:${command.id}>`
                                 : 'No name',
-                            value: `${
-                                subOption.description
-                                    ? subOption.description
-                                    : 'No description'
-                            }`,
+                            value: `${command.description
+                                ? command.description
+                                : 'No description'
+                                }`,
                         })
+                    }
+                    command.options?.forEach((option) => {
+                        if (option.type == 2) {
+                            option.options?.forEach((subOption) => {
+                                embed.addFields({
+                                    name: `</${command.name +
+                                        ' ' +
+                                        option.name +
+                                        ' ' +
+                                        subOption.name
+                                        }:${command.id}>`
+                                        ? `</${command.name +
+                                        ' ' +
+                                        option.name +
+                                        ' ' +
+                                        subOption.name
+                                        }:${command.id}>`
+                                        : 'No name',
+                                    value: `${subOption.description
+                                        ? subOption.description
+                                        : 'No description'
+                                        }`,
+                                })
+                            })
+                        }
+                        return
                     })
-                }
-                return
+                })
             })
+            embeds.push(embed)
         })
 
+
         await interaction.reply({
-            embeds: [helpEmbed],
+            embeds: [embeds[0], embeds[1], embeds[2]],
             ephemeral: true,
         })
     },
