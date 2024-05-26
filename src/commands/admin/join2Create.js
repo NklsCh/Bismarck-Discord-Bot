d9c4f591e8ce47db42369cd302dce021dbaf6dc3
Niklas Choinowki <niklas.choinowski@gmail.com>
2024-05-26 20:15:38 +0200

    feat: Add Join2Create system functionality
    The code changes include adding the necessary database column `join2CreateChannelId` to the `guilds` model and updating the translation files to support the Join2Create system. This commit adds the functionality for setting up the Join2Create system, including a command to specify the channel for Join2Create messages and corresponding activation and deactivation replies.
    
    Ref: Join2Create #57

---
+++ src/commands/admin/join2Create.js
@@ -0,0 +1,59 @@
+const {
+    PermissionFlagsBits: { Administrator },
+    SlashCommandBuilder,
+    ChatInputCommandInteraction
+} = require('discord.js')
+const Guilds = require('../../../models/guilds')
+
+const langData = require(`../../../resources/translations/lang.json`)
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('join2create')
+        .setDescription(langData.en.join2Create.command.description)
+        .setDescriptionLocalizations({
+            de: langData.de.join2Create.command.description,
+        })
+        .addChannelOption((option) =>
+            option
+                .setName('channel')
+                .setDescription(langData.en.join2Create.command.channelOptionDescription)
+                .setDescriptionLocalizations({
+                    de: langData.de.join2Create.command.channelOptionDescription,
+                })
+        )
+        .setDefaultMemberPermissions(Administrator)
+        .setDMPermission(false),
+    /**
+     * @param {ChatInputCommandInteraction} interaction - The interaction object.
+     * @returns {Promise<void>}
+     */
+    async execute(interaction) {
+        const userLang = interaction.locale.slice(0, 2)
+        const [dbGuild] = await Guilds.findOrCreate({
+            where: { guildId: interaction.guild.id },
+        })
+
+        const channel = interaction.options.getChannel('channel')
+
+        if (!channel) {
+            await interaction.reply({
+                content: langData[userLang].join2Create.reply.deactivated,
+                ephemeral: true,
+            }).then(async () => {
+                await dbGuild.update({
+                    join2CreateChannelId: null,
+                })
+                return
+            })
+
+        }
+        await interaction.reply({
+            content: langData[userLang].join2Create.reply.activated,
+            ephemeral: true,
+        })
+        await dbGuild.update({
+            join2CreateChannelId: channel.id,
+        })
+    },
+}
