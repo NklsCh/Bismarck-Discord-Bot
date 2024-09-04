48a58c4385f3c1588255978e82c6bade004a7442
Niklas Choinowski <niklas.choinowski@develogment.com>
2024-09-04 11:33:42 +0200

    refactor: Update join2Create command to use consistent code formatting
    The join2Create command file has been updated to use consistent code formatting. This improves code readability and maintainability.
    
    Refactor the join2Create command to adhere to consistent code formatting.
    
    Ref: N/A

--- src/commands/admin/join2Create.js
+++ src/commands/admin/join2Create.js
@@ -2,58 +2,58 @@ const {
     PermissionFlagsBits: { Administrator },
     SlashCommandBuilder,
     ChatInputCommandInteraction
-} = require('discord.js')
-const Guilds = require('../../../models/guilds')
+} = require( 'discord.js' )
+const Guilds = require( '../../../models/guilds' )
 
-const langData = require(`../../../resources/translations/lang.json`)
+const langData = require( `../../../resources/translations/lang.json` )
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('join2create')
-        .setDescription(langData.en.join2Create.command.description)
-        .setDescriptionLocalizations({
+        .setName( 'join2create' )
+        .setDescription( langData.en.join2Create.command.description )
+        .setDescriptionLocalizations( {
             de: langData.de.join2Create.command.description,
-        })
-        .addChannelOption((option) =>
+        } )
+        .addChannelOption( ( option ) =>
             option
-                .setName('channel')
-                .setDescription(langData.en.join2Create.command.channelOptionDescription)
-                .setDescriptionLocalizations({
+                .setName( 'channel' )
+                .setDescription( langData.en.join2Create.command.channelOptionDescription )
+                .setDescriptionLocalizations( {
                     de: langData.de.join2Create.command.channelOptionDescription,
-                })
+                } )
         )
-        .setDefaultMemberPermissions(Administrator)
-        .setDMPermission(false),
+        .setDefaultMemberPermissions( Administrator )
+        .setDMPermission( false ),
     /**
      * @param {ChatInputCommandInteraction} interaction - The interaction object.
      * @returns {Promise<void>}
      */
-    async execute(interaction) {
-        const userLang = interaction.locale.slice(0, 2)
-        const [dbGuild] = await Guilds.findOrCreate({
+    async execute( interaction ) {
+        const userLang = interaction.locale.slice( 0, 2 )
+        const [ dbGuild ] = await Guilds.findOrCreate( {
             where: { guildId: interaction.guild.id },
-        })
+        } )
 
-        const channel = interaction.options.getChannel('channel')
+        const channel = interaction.options.getChannel( 'channel' )
 
-        if (!channel) {
-            await interaction.reply({
-                content: langData[userLang].join2Create.reply.deactivated,
+        if ( !channel ) {
+            await interaction.reply( {
+                content: langData[ userLang ].join2Create.reply.deactivated,
                 ephemeral: true,
-            }).then(async () => {
-                await dbGuild.update({
+            } ).then( async () => {
+                await dbGuild.update( {
                     join2CreateChannelId: null,
-                })
+                } )
                 return
-            })
+            } )
 
         }
-        await interaction.reply({
-            content: langData[userLang].join2Create.reply.activated,
+        await interaction.reply( {
+            content: langData[ userLang ].join2Create.reply.activated,
             ephemeral: true,
-        })
-        await dbGuild.update({
+        } )
+        await dbGuild.update( {
             join2CreateChannelId: channel.id,
-        })
+        } )
     },
 }
